38517991e5443ab0962ce5c9b7f95d1121892e62
bdarge <bin.darge@gmail.com>
2023-05-22 20:36:58 -0400

    add new forms (actual)

---
+++ src/app/business/aboutme/aboutme.component.ts
@@ -0,0 +1,117 @@
+import { Component } from '@angular/core';
+import { ROUTE_ANIMATIONS_ELEMENTS } from "../../core/animations/route.animations";
+import { UntypedFormBuilder, UntypedFormControl, UntypedFormGroup, ValidationErrors, Validators } from "@angular/forms";
+import { ConfigWebService } from "../../http/config-web.service";
+import { NotificationService } from "../../core/notifications/notification.service";
+import { LocalStorageService } from "../../core/local-storage/local-storage.service";
+import { Address, Business, BusinessAddress, User } from "../../model/user";
+import { debounceTime, distinctUntilChanged } from "rxjs/operators";
+import { Account } from "../../model/account";
+
+@Component({
+  selector: 'app-aboutme',
+  templateUrl: './aboutme.component.html',
+  styleUrls: ['./aboutme.component.scss']
+})
+export class AboutmeComponent {
+
+  protected readonly ROUTE_ANIMATIONS_ELEMENTS = ROUTE_ANIMATIONS_ELEMENTS;
+
+  businessForm = this.fb.group({
+    id: [''],
+    name: ['', [Validators.required]],
+    address: {
+      id: [''],
+      street: ['',],
+      postalCode: [''],
+      city: ['',],
+      country: ['',],
+      landline: [''],
+      mobile: [''],
+    },
+    hourlyRate: [''],
+    vat: [''],
+  })
+
+  constructor(private fb: UntypedFormBuilder,
+              private configService: ConfigWebService,
+              private notificationService: NotificationService,
+              private localStorageSvc: LocalStorageService) {
+
+  }
+
+  ngOnInit() {
+    const acct = this.localStorageSvc.getItem('ACCOUNT') as Account
+    const adminRoles = [1, 2]
+    let isAdmin : boolean = false
+    this.configService.getBusiness(acct.businessId)
+      .subscribe(data => {
+        if (!acct.roles.includes("admin")) {
+          this.businessForm.disable({ onlySelf: true });
+        }
+        let d = {
+          id: data.id,
+          name: data.name,
+          vat: data.vat,
+          hourlyRate: data.hourlyRate,
+          address: {
+            id: data.id,
+            landline: data.landline,
+            street: data.street,
+            city: data.city,
+            postalCode: data.postalCode,
+            country: data.country,
+            mobile: data.mobile || ""
+          } as BusinessAddress
+        }
+        console.log(d)
+        this.businessForm.patchValue(d)
+
+        this.businessForm.valueChanges.pipe(
+          debounceTime(1000),
+          distinctUntilChanged((a, b) => JSON.stringify(a) === JSON.stringify(b)))
+          .subscribe((f) => {
+            this.save()
+          })
+      });
+  }
+
+  save() {
+    if (this.businessForm.valid) {
+      console.log("...", this.businessForm.value)
+      let m = {...this.businessForm.value, ...this.businessForm.value.address}
+      delete m.address
+      this.configService.saveBusiness(m)
+        .subscribe((business) => {
+          if (business) {
+            this.businessForm.patchValue(business,  { emitEvent: false });
+            this.notificationService.info('saved')
+          }
+        }, err => {
+          this.notificationService.error(err ? err.message : 'failed to save. ' + err)
+        });
+    } else {
+      this.validateAll(this.businessForm)
+      this.notificationService.error('form is invalid.')
+    }
+  }
+
+  validateAll(businessForm) {
+    Object.keys(businessForm.controls).forEach(key => {
+      const controlErrors: ValidationErrors = this.businessForm.get(key).errors
+      if (controlErrors != null) {
+        Object.keys(controlErrors).forEach(keyError => {
+          console.log('Key control: ' + key + ', keyError: ' + keyError +
+            ', err value: ', controlErrors[keyError])
+        })
+      }
+
+      const control = this.businessForm.get(key)
+      if (control instanceof UntypedFormControl) {
+        control.markAsTouched({onlySelf: true})
+      } else if (control instanceof UntypedFormGroup) {
+        this.validateAll(control)
+      }
+    })
+  }
+}
---
+++ src/app/business/address-form/address-form.component.ts
@@ -0,0 +1,102 @@
+import { Component, OnDestroy, OnInit, Self } from "@angular/core";
+import {
+  AbstractControl,
+  ControlValueAccessor,
+  FormBuilder,
+  FormControl,
+  FormGroup, NG_VALIDATORS, NG_VALUE_ACCESSOR,
+  Validator
+} from "@angular/forms";
+import { ROUTE_ANIMATIONS_ELEMENTS } from '../../core/core.module';
+import { Subscription } from "rxjs";
+
+@Component({
+  selector: 'address-form',
+  templateUrl: './address-form.component.html',
+  styleUrls: ['./address-form.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: AddressFormComponent
+    },
+    {
+      provide: NG_VALIDATORS,
+      multi: true,
+      useExisting: AddressFormComponent
+    },
+  ]
+})
+export class AddressFormComponent implements OnInit, ControlValueAccessor, OnDestroy, Validator  {
+  routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS
+  form = new FormGroup({
+    id: new FormControl<string>(''),
+    street: new FormControl<string>(''),
+    postalCode: new FormControl<string>(''),
+    city: new FormControl<string>(''),
+    country: new FormControl<string>(''),
+    landline: new FormControl<string>(''),
+    mobile: new FormControl<string>(''),
+  })
+
+  onTouched: Function = () => {};
+
+  onChangeSubs: Subscription[] = [];
+
+  constructor(private fb: FormBuilder) {
+  }
+  ngOnDestroy(): void {
+    for (let sub of this.onChangeSubs) {
+      sub.unsubscribe();
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+  registerOnChange(fn: any): void {
+    const sub = this.form.valueChanges.subscribe(fn);
+    this.onChangeSubs.push(sub);
+  }
+
+  registerOnTouched(fn: Function): void {
+    this.onTouched = fn;
+  }
+
+  writeValue(value: any): void {
+    value && this.form.setValue(value, { emitEvent: false });
+  }
+
+  setDisabledState(disabled: boolean): void {
+    disabled ? this.form.disable() : this.form.enable();
+  }
+
+  validate(control: AbstractControl) {
+
+    if (this.form.valid) {
+      return null;
+    }
+
+    let errors : any = {};
+
+    errors = this.addControlErrors(errors, "street");
+    errors = this.addControlErrors(errors, "postalCode");
+    errors = this.addControlErrors(errors, "city");
+    errors = this.addControlErrors(errors, "mobile");
+    errors = this.addControlErrors(errors, "phone");
+    return errors;
+  }
+
+  addControlErrors(allErrors: any, controlName:string) {
+
+    const errors = {...allErrors};
+
+    const controlErrors = this.form.controls[controlName].errors;
+
+    if (controlErrors) {
+      errors[controlName] = controlErrors;
+    }
+
+    return errors;
+  }
+}
