450a1e1cca729748f0e57c024835d8a1f033a2e0
bdarge <bin.darge@gmail.com>
2025-06-25 22:37:01 -0400

    revert testing calls

--- src/app/http/sb.service.ts
+++ src/app/http/sb.service.ts
@@ -15,21 +15,6 @@ export class SbService {
   }
 
   convert(req: CurrencyRequest): Observable<Currency> {
-    const d = {
-      'SEK': {
-        to: 'sek',
-        base: 'usd',
-        value: 9.4
-      },
-      'USD': {
-        to: 'usd',
-        base: 'sek',
-        value: 1
-      }
-    }
-    console.log("...", req)
-    // return this.http.post<Currency>(this.URL + '/currency', req);
-    return timer(1000).pipe(switchMap(()=> of(d[req.symbol.valueOf()])));
-    // return of(d[req.symbol.valueOf()])
+    return this.http.post<Currency>(this.URL + '/currency', req);
   }
 }
