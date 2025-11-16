a9a1db93a3b6314730442c341fcc751f3dc3fa09
bdarge <bin.darge@gmail.com>
2025-11-16 10:57:02 -0500

    edit btn was removed

--- src/app/business/customer/customer.component.spec.ts
+++ src/app/business/customer/customer.component.spec.ts
@@ -1,24 +1,27 @@
-import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
-import {CustomerComponent} from './customer.component';
-import {MatButtonHarness} from '@angular/material/button/testing';
-import {MatDialogHarness} from '@angular/material/dialog/testing';
-import {HarnessLoader} from '@angular/cdk/testing';
-import {SharedModule} from '../../shared/shared.module';
-import {TranslateModule} from '@ngx-translate/core';
-import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {TestbedHarnessEnvironment} from '@angular/cdk/testing/testbed';
-import {EditCustomerComponent} from '../edit-customer/edit-customer.component';
-import {NotificationService} from '../../core/notifications/notification.service';
-import {MatDialog} from '@angular/material/dialog';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
-import {PageRequest, Page, Query} from '../../model/page';
-import {from, Observable, of} from 'rxjs';
-import {Customer} from '../../model/customer';
-import { FontAwesomeIconsModule } from 'app/shared/font.awesome.icons.module';
-import {CustomerWebService} from '../../http/customer-web.service';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
+import { CustomerComponent } from "./customer.component";
+import { MatButtonHarness } from "@angular/material/button/testing";
+import { MatDialogHarness } from "@angular/material/dialog/testing";
+import { HarnessLoader } from "@angular/cdk/testing";
+import { SharedModule } from "../../shared/shared.module";
+import { TranslateModule } from "@ngx-translate/core";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { TestbedHarnessEnvironment } from "@angular/cdk/testing/testbed";
+import { EditCustomerComponent } from "../edit-customer/edit-customer.component";
+import { NotificationService } from "../../core/notifications/notification.service";
+import { MatDialog } from "@angular/material/dialog";
+import { provideHttpClientTesting } from "@angular/common/http/testing";
+import { PageRequest, Page, Query } from "../../model/page";
+import { from, Observable, of } from "rxjs";
+import { Customer } from "../../model/customer";
+import { FontAwesomeIconsModule } from "app/shared/font.awesome.icons.module";
+import { CustomerWebService } from "../../http/customer-web.service";
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from "@angular/common/http";
 
-describe('CustomerComponent', () => {
+describe("CustomerComponent", () => {
   let component: CustomerComponent;
   let fixture: ComponentFixture<CustomerComponent>;
   let customerService: CustomerWebService;
@@ -26,46 +29,56 @@ describe('CustomerComponent', () => {
   let rootLoader: HarnessLoader;
   let customerServiceStub: Partial<CustomerWebService>;
 
-  const getAddButton = async () => loader.getHarness<MatButtonHarness>(MatButtonHarness);
+  const getAddButton = async () =>
+    loader.getHarness<MatButtonHarness>(MatButtonHarness);
 
-  const getDialog = async () =>  rootLoader.getHarness<MatDialogHarness>(MatDialogHarness);
+  const getDialog = async () =>
+    rootLoader.getHarness<MatDialogHarness>(MatDialogHarness);
 
   beforeEach(waitForAsync(() => {
     customerServiceStub = {
       page(request: PageRequest, query: Query): Observable<Page<Customer>> {
-        return from(new Promise<Page<Customer>>((resolve, reject) => {
-          resolve({
-            data: [{
-              id: '1',
-              name: 'alex'
-            } as Customer],
-            total: 1,
-            limit: request.size,
-            page: request.page
-          } as Page<Customer>)
-        }))
-      }
+        return from(
+          new Promise<Page<Customer>>((resolve, reject) => {
+            resolve({
+              data: [
+                {
+                  id: "1",
+                  name: "alex",
+                } as Customer,
+              ],
+              total: 1,
+              limit: request.size,
+              page: request.page,
+            } as Page<Customer>);
+          }),
+        );
+      },
     };
 
     TestBed.configureTestingModule({
-    declarations: [CustomerComponent, EditCustomerComponent],
-    imports: [SharedModule,
+      declarations: [CustomerComponent, EditCustomerComponent],
+      imports: [
+        SharedModule,
         FontAwesomeIconsModule,
         NoopAnimationsModule,
-        TranslateModule.forRoot()],
-    providers: [
+        TranslateModule.forRoot(),
+      ],
+      providers: [
         {
-            provide: NotificationService
-        }, {
-            provide: CustomerWebService, useValue: customerServiceStub
-        }, {
-            provide: MatDialog
+          provide: NotificationService,
+        },
+        {
+          provide: CustomerWebService,
+          useValue: customerServiceStub,
+        },
+        {
+          provide: MatDialog,
         },
         provideHttpClient(withInterceptorsFromDi()),
-        provideHttpClientTesting()
-    ]
-})
-      .compileComponents();
+        provideHttpClientTesting(),
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -79,17 +92,17 @@ describe('CustomerComponent', () => {
     rootLoader = TestbedHarnessEnvironment.documentRootLoader(fixture);
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 
-  it('should add button available', async () => {
+  it("should add button available", async () => {
     fixture.detectChanges();
     const removeDoneTodosButton = await getAddButton();
     expect(removeDoneTodosButton).toBeTruthy();
   });
 
-  it('should open a dialog when add button clicked', async () => {
+  it("should open a dialog when add button clicked", async () => {
     fixture.detectChanges();
     const addButton = await getAddButton();
     await addButton.click();
@@ -97,16 +110,6 @@ describe('CustomerComponent', () => {
     const dialogs = await rootLoader.getAllHarnesses(MatDialogHarness);
     expect(dialogs.length).toBe(1);
     expect(dialogHarness).toBeTruthy();
-    await dialogHarness.close()
+    await dialogHarness.close();
   });
-
-  it('should open a dialog when edit button clicked', async () => {
-      const editButton = await loader.getHarness(MatButtonHarness.with({selector: 'button[aria-label=edit]'}));
-      await editButton.click()
-      const dialogHarness = await getDialog();
-      const dialogs = await rootLoader.getAllHarnesses(MatDialogHarness);
-      expect(dialogs.length).toBe(1);
-      expect(dialogHarness).toBeTruthy();
-      await dialogHarness.close();
-    });
 });
